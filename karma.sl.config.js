97640c1b169a59bb7c29af9f93d74e728a4de34c
Pawel Psztyc <jarrodek@gmail.com>
2019-12-22 22:54:18 -0800

    test: termporaily disabling non-chrome tests

--- karma.sl.config.js
+++ karma.sl.config.js
@@ -12,10 +12,10 @@ module.exports = (config) => {
   slConfig.browsers = [
     'SL_Chrome',
     'SL_Chrome-1',
-    'SL_Firefox',
-    'SL_Firefox-1',
-    'SL_Safari',
-    'SL_Safari-1',
+    // 'SL_Firefox',
+    // 'SL_Firefox-1',
+    // 'SL_Safari',
+    // 'SL_Safari-1',
     // 'SL_EDGE',
   ];
   config.set(merge(createBaseConfig(config), slConfig));
